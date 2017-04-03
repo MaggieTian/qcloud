36d45b80d4b3e40de3ff9798fb77a00f01e87ccc
MaggieTian <1729430864@qq.com>
2017-04-03 11:39:33 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,18 @@
 
 const express = require('express');
 const router = express.Router();
+var mongoose = require('mongoose');
 
 router.get('/', require('./welcome'));
 router.get('/login', function(req,res){
-		res.send("hello maggie tian!");
+		mongoose.connect("mongodb://localhost/hospital");
+        var db=mongoose.connection;
+
+        db.on('error', console.error.bind(console, 'connection error:'));
+        db.once('open', function() {
+            console.log("db  connection success!");
+            res.send("hello maggie!");
+        });
 }
 );
 router.get('/user', require('./user'));
